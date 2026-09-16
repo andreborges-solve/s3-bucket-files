7e2af30a6398250b53f3cf3e9b54f45dfc4454bd
andreborges-solve <andre.borges@solve4me.com.br>
2026-09-16 10:56:58 -0300

    config ports to prd

--- s3-bucket-app/vite.config.ts
+++ s3-bucket-app/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  // No Docker passamos BACKEND_URL=http://backend:3000. Fora do Docker usa localhost:3000
   const backendTarget = process.env.BACKEND_URL || env.BACKEND_URL || 'http://localhost:3000';
 
   const proxyConfig = {
